e3106bfa946a69c9dc2f32dd8dfb92ad877877ff
Andrey <2593147@gmail.com>
2022-01-25 12:11:50 +0200

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,45 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  const codeMorseMap = {
+    10: '.',
+    11: '-',
+  };
+
+  const doubleEncodedWords = [];
+  for (let i = 0; i < expr.length; i += 10) {
+    const part = expr.slice(i, i + 10);
+    if (part.startsWith('*')) {
+      doubleEncodedWords.push(' ');
+    } else {
+      doubleEncodedWords.push(part);
+    }
+  }
+
+  const normalizedWords = doubleEncodedWords.map((el) => {
+    const oneIdx = el.indexOf('1');
+    if (oneIdx > 0) {
+      return el.substring(oneIdx);
+    }
+    return el;
+  });
+
+  const morseEncodedWords = normalizedWords.map((word) => {
+    if (word.length <= 1) {
+      return word;
+    }
+
+    const morseWord = [];
+    for (let i = 0; i < word.length; i += 2) {
+      const subStr = word.substr(i, 2);
+      morseWord.push(codeMorseMap[subStr]);
+    }
+    return morseWord.join('');
+  });
+
+  const decodedStr = morseEncodedWords.map((el) => MORSE_TABLE[el] || el).join('');
+
+  return decodedStr;
 }
 
 module.exports = {
